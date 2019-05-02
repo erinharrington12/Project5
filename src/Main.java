f8536e5b6b31bf57fb15b4ff1756e305ee9fe7b4
ErinHarrington <erin.c.harrington-1@ou.edu>
2019-05-01 20:11:48 -0500

    Added new panels for organization

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,25 @@
-<<<<<<< HEAD
+
 import java.awt.GridLayout;
 import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-=======
+
 import java.io.IOException;
 
 import javax.swing.JFrame;
->>>>>>> fe83d91344740ec73c6e78d61ebd07450b8416b8
+
 
 public class Main
 {
 private static Panel drawPanel;
     
     public static void main(String[] args) throws IOException {
-        
-<<<<<<< HEAD
-        Frame frame = new Frame();
-        
-       
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
 
       
-=======
+
+      
+
         Frame appFrame = new Frame();
         
         appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,7 +32,7 @@ private static Panel drawPanel;
         
         
         
->>>>>>> fe83d91344740ec73c6e78d61ebd07450b8416b8
+
         
     }
 }
